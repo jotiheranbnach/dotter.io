b2215751159fcdc4dbdaff5bbbd29d011db2f6bf
jotiheranbnach <johann.tierbach@gmail.com>
2019-10-17 10:53:08 +0200

    Updated the tsconfig and retranspiled the project.

--- src/dotter.js
+++ src/dotter.js
@@ -1,81 +1,76 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 require("../assets/scss/styles.css");
-var Dot_1 = require("./Dot");
-var Snake_1 = require("./Snake");
-var Ellipse_1 = require("./Ellipse");
-var Dotter = /** @class */ (function () {
-    function Dotter() {
+const Dot_1 = require("./Dot");
+const Snake_1 = require("./Snake");
+const Ellipse_1 = require("./Ellipse");
+class Dotter {
+    constructor() {
         this.dots = [];
     }
-    Dotter.prototype.main = function () {
+    main() {
         this.initDomReferences();
         this.prepareCanvasElement();
         this.initCanvasHandles();
         // this.runDotExchange();
         this.runSpherePulse();
-    };
-    Dotter.prototype.runSpherePulse = function () {
-        var _this = this;
-        var circleDiameter = 600;
-        var r = circleDiameter / 2;
-        var step = 25;
-        var generateEllipses = function () {
-            var result = [];
-            for (var i = step, id = 0; i < circleDiameter; i += step, id++) {
-                var ellipse = new Ellipse_1.Ellipse(i - circleDiameter / 2, 0, step * 1.8, Math.sqrt((r * r) - ((r - i) * (r - i))), id);
+    }
+    runSpherePulse() {
+        let circleDiameter = 600;
+        let r = circleDiameter / 2;
+        let step = 25;
+        let generateEllipses = () => {
+            let result = [];
+            for (let i = step, id = 0; i < circleDiameter; i += step, id++) {
+                let ellipse = new Ellipse_1.Ellipse(i - circleDiameter / 2, 0, step * 1.8, Math.sqrt((r * r) - ((r - i) * (r - i))), id);
                 result.push(ellipse);
             }
             return result;
         };
-        var drawEllipses = function (ellipses) {
-            for (var _i = 0, ellipses_1 = ellipses; _i < ellipses_1.length; _i++) {
-                var ellipse = ellipses_1[_i];
-                ellipse.drawEllipse(_this.ctx);
+        let drawEllipses = (ellipses) => {
+            for (let ellipse of ellipses) {
+                ellipse.drawEllipse(this.ctx);
             }
         };
-        var ellipses = generateEllipses();
+        let ellipses = generateEllipses();
         this.ctx.translate(this.canvasElement.width / 2, this.canvasElement.height / 2);
         this.ctx.rotate(Math.PI * 4 / 3.2);
-        setInterval(function () {
-            _this.ctx.clearRect(-_this.canvasElement.width, -_this.canvasElement.height, _this.canvasElement.width * 2, _this.canvasElement.height * 2);
+        setInterval(() => {
+            this.ctx.clearRect(-this.canvasElement.width, -this.canvasElement.height, this.canvasElement.width * 2, this.canvasElement.height * 2);
             drawEllipses(ellipses);
         }, 17);
-    };
-    Dotter.prototype.moveX = function (x, axisLength) {
+    }
+    moveX(x, axisLength) {
         return x + (this.canvasElement.width / 2) - axisLength / 2;
-    };
-    Dotter.prototype.generateDots = function () {
-        var rowHeight = this.canvasElement.height / 50;
+    }
+    generateDots() {
+        let rowHeight = this.canvasElement.height / 50;
         this.rectSize = rowHeight;
         this.snake = new Snake_1.Snake(50, Math.round(this.canvasElement.width / rowHeight));
-        for (var row = 0, y = 0; y < this.canvasElement.height; y += rowHeight, row++) {
+        for (let row = 0, y = 0; y < this.canvasElement.height; y += rowHeight, row++) {
             this.dots[row] = [];
-            for (var column = 0, x = 0; x < this.canvasElement.width; x += this.rectSize, column++) {
+            for (let column = 0, x = 0; x < this.canvasElement.width; x += this.rectSize, column++) {
                 this.dots[row][column] = new Dot_1.Dot(row, column);
             }
         }
-    };
-    Dotter.prototype.drawDots = function () {
+    }
+    drawDots() {
         this.ctx.clearRect(0, 0, this.canvasElement.width, this.canvasElement.height);
-        var margin = this.rectSize - (this.rectSize * .7);
-        for (var row = 0; row < this.dots.length; row++) {
-            for (var column = 0; column < this.dots[0].length; column++) {
+        let margin = this.rectSize - (this.rectSize * .7);
+        for (let row = 0; row < this.dots.length; row++) {
+            for (let column = 0; column < this.dots[0].length; column++) {
                 this.ctx.fillStyle = 'rgb(' + 150 + ',' + 50 + ',' + 50 + ')';
                 this.ctx.fillRect((column * this.rectSize) + margin, (row * this.rectSize) + margin, this.rectSize - margin, this.rectSize - margin);
             }
         }
-        for (var _i = 0, _a = this.snake.body; _i < _a.length; _i++) {
-            var snakePart = _a[_i];
+        for (let snakePart of this.snake.body) {
             this.ctx.fillStyle = 'rgb(' + 255 + ',' + 150 + ',' + 150 + ')';
             this.ctx.fillRect((snakePart.column * this.rectSize) + margin, (snakePart.row * this.rectSize) + margin, this.rectSize - margin, this.rectSize - margin);
         }
-    };
-    Dotter.prototype.runDotExchange = function () {
-        var _this = this;
+    }
+    runDotExchange() {
         this.generateDots();
-        setInterval(function () {
-            _this.drawDots();
+        setInterval(() => {
+            this.drawDots();
             // let getRandomBetween = (min: number, max: number) => {
             //     return Math.round(Math.random() * (max - min) + min);
             // };
@@ -85,26 +80,25 @@ var Dotter = /** @class */ (function () {
             // let randomDot2: Dot = this.dots[getRandomBetween(0, this.dots.length)][getRandomBetween(0, this.dots[0].length)];
             // randomDot2.isInTransit = false;
         }, 100);
-    };
-    Dotter.prototype.initDomReferences = function () {
+    }
+    initDomReferences() {
         this.canvasElement = document.getElementById(Dotter.canvas_id);
         this.outerLimitsElement = document.getElementById(Dotter.outer_limits_id);
-    };
-    Dotter.prototype.prepareCanvasElement = function () {
+    }
+    prepareCanvasElement() {
         this.outerLimitsRect = this.outerLimitsElement.getBoundingClientRect();
         this.canvasElement.width = this.outerLimitsRect.width;
         this.canvasElement.height = this.outerLimitsRect.height;
-    };
-    Dotter.prototype.initCanvasHandles = function () {
+    }
+    initCanvasHandles() {
         this.ctx = this.canvasElement.getContext("2d");
         this.ctx.clearRect(0, 0, this.canvasElement.width, this.canvasElement.height);
-    };
-    Dotter.canvas_id = 'dotter-canvas';
-    Dotter.outer_limits_id = 'outer-limits';
-    return Dotter;
-}());
-(function () {
-    var dotter = new Dotter();
+    }
+}
+Dotter.canvas_id = 'dotter-canvas';
+Dotter.outer_limits_id = 'outer-limits';
+(() => {
+    const dotter = new Dotter();
     dotter.main();
 })();
 //# sourceMappingURL=dotter.js.map
